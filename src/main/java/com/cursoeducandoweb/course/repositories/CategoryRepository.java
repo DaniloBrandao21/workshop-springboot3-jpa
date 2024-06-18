358552722e7cf9e18180c7dcba9ff4fb3b392d58
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-18 14:50:57 -0300

    Category

---
+++ src/main/java/com/cursoeducandoweb/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.cursoeducandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursoeducandoweb.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+	
+
+}
