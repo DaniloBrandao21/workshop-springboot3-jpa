556d2bc2b09b139e035bc652bf039514be0ef379
Danilo Brandão <danilo.brandao.ifpr@gmail.com>
2024-06-20 10:11:02 -0300

    Update user

--- src/main/java/com/cursoeducandoweb/course/resources/UserResource.java
+++ src/main/java/com/cursoeducandoweb/course/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.cursoeducandoweb.course.entities.User;
 import com.cursoeducandoweb.course.services.UserService;
 
-import jakarta.servlet.Servlet;
+import jakarta.persistence.PostUpdate;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -52,6 +53,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity <User> update(@PathVariable Long id,@RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
